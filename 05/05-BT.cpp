1b559230bd550c28230b19c97442741b270e575b
QuangDinh999 <92561923+QuangDinh999@users.noreply.github.com>
2021-10-28 15:36:13 +0700

    btvn

---
+++ 05/05-BT.cpp
@@ -0,0 +1,36 @@
+// 05.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include < string>
+using namespace std;
+
+int main()
+{
+	nt m;
+	int n;
+	cout << " chon 2 so nguyen " << '\n';
+	cin >> m;
+	cin >> n;
+	int a = 1;
+	int b = 1;
+	for (int i = 1; i <= m; ++i)
+		a *= i;
+
+	for (int i = 1; i <= n; ++i)
+		b *= i;
+
+	cout << " m! + n! =: " << a + b << '\n';
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
